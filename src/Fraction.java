c3d97f0c94109b95c2c6f06ed1d5791ba8c4eac2
Boutouili Djillali <gd_boutouili@esi.dz>
2021-09-30 14:40:24 +0200

    dernier commit

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,5 +1,12 @@
 
 public class Fraction {
 
+	public int nominateur;
+	public int denominateur;
+	public Fraction(int nominateur,int denominateur) {
+		this.nominateur = nominateur;
+		this.denominateur = denominateur;
+	}
+	
 	
 }
--- src/Mainclass.java
+++ src/Mainclass.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 public class Mainclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
+		 Scanner scanner = new Scanner(System.in);
+		 
+            int nominateur;
+            int denominateur;
+    
+            System.out.print("Enter nominateur: ");
+            nominateur = scanner.nextInt();
+            System.out.print("Enter denominateur: ");
+            denominateur = scanner.nextInt();
+            Fraction fraction = new Fraction(nominateur,denominateur);
+
+            assert fraction.denominateur != 0  : "Exception lancee"; 
+
+            System.out.printf( "Fraction => %s", fraction.nominateur/fraction.denominateur);
+            scanner.close();
      
 	}
 
